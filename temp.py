d452cafc352b3d2f06086622d47fcda077bf1e3f
thotgamma <gamma@e.thotgamma.com>
2016-10-26 10:39:33 +0900

    いろいろした

--- temp.py
+++ temp.py
@@ -2,18 +2,27 @@
 # -*- coding: utf-8 -*-
 
 import sys
-import os.path
+import os
 import re
 
 
 args = sys.argv #コマンドライン引数
+if(len(sys.argv) == 1):
+    print("ファイル名を書けつってんの")
+    sys.exit()
 filename = extension = args[1].split(".")[0]
 extension = args[1].split(".")[-1]
 
 target = open(args[1], 'w')
 
-if(os.path.isfile(extension + ".temp")):
-    template = open(extension + ".temp", 'r')
+
+if(os.path.isfile(os.environ['HOME'] + "/" + extension + ".temp")):
+    templatePath = os.environ['HOME'] + "/" + extension + ".temp"
+elif(os.path.isfile("templates/" + extension + ".temp")):
+    templatePath = "templates/" + extension + ".temp"
+
+if 'templatePath' in locals():
+    template = open(templatePath, 'r')
     for line in template:
         replacedLine = line.replace("<#filename#>", filename)
         replacedLine = line.replace("<#FILENAME#>", filename.upper())
